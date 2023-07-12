83b03f32640889a1abc4d43cf835b116bfad051b
Ajay Mudunuri <ajay.mudunuri@yahoo.com>
2023-07-12 15:17:00 -0400

    Converting file handling to C++ functions. Adding support for date & timestamps

--- hello_beagle_bone.cpp
+++ hello_beagle_bone.cpp
@@ -11,37 +11,74 @@
  */
 
 #include <iostream>
-#include <stdio.h>
+#include <fstream>
 #include <unistd.h>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
+
 using namespace std;
+using namespace chrono;
+
+void addDate(void);
+void addTimeStamp(void);
 
 int main(void)
 {
     cout << "Hello Beagle Bone from Host PC" << endl;
 
-    FILE *LEDHandle = NULL;
-    char *LEDBrightness = "/sys/devices/platform/leds/leds/beaglebone:green:usr3/brightness";
+    ofstream LEDHandle;
+    string LEDBrightness = "/sys/devices/platform/leds/leds/beaglebone:green:usr3/brightness";
 
+    addDate();
+    addTimeStamp();
     cout << "LED Flash start..." << endl;
 
     for(int i=0; i < 10; i++)
     {
-        if((LEDHandle = fopen(LEDBrightness, "r+"))!=NULL)
+        addTimeStamp();
+        cout << "Turning LED On..." << endl;
+        LEDHandle.open(LEDBrightness);
+        if(LEDHandle)
         {
-            fwrite("1", sizeof(char), 1, LEDHandle);
-            fclose(LEDHandle);
+            LEDHandle << "1" << endl;
+            LEDHandle.close();
         }
         sleep(1);
 
-        if((LEDHandle = fopen(LEDBrightness, "r+"))!=NULL)
+        addTimeStamp();
+        cout << "Turning LED Off..." << endl;
+        LEDHandle.open(LEDBrightness);
+        if(LEDHandle)
         {
-            fwrite("0", sizeof(char), 1, LEDHandle);
-            fclose(LEDHandle);
+            LEDHandle << "0" << endl;
+            LEDHandle.close();
         }
         sleep(1);
     }
 
+    addDate();
+    addTimeStamp();
     cout << "LED Flash end" << endl;
 
     return 0;
+}
+
+void addDate(void)
+{
+    auto now = system_clock::now();
+    time_t now_time = (system_clock::to_time_t(now));
+    auto currentTime = gmtime(&now_time);
+    auto datestamp = put_time(currentTime, "%Y-%m-%d_");
+    cout << datestamp ;
+}
+
+void addTimeStamp(void)
+{
+    auto now = system_clock::now();
+    auto milliSec = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+    time_t now_time = (system_clock::to_time_t(now));
+    auto currentTime = gmtime(&now_time);
+    auto timestamp = put_time(currentTime, "%H:%M:%S");
+    cout << timestamp <<  '.' << setfill('0') << setw(3) << milliSec.count() << " : ";
 }
